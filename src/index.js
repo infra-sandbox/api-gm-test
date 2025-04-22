d77917506c3de96bd4bb06974af0a0ba9014bc93
thecariah <carolina.antillanca@usach.cl>
2025-04-22 17:14:54 -0400

    Enable CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const pool = require('./db');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+app.use(cors());
 app.use(express.json());
 
 // GET: obtener registros
